7fe93935cb31602f14b170128a7cebb87c4e16d2
jermspeaks <jermspeaks@gmail.com>
2014-08-09 13:37:01 -0500

    created a signup form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,26 @@
 get '/' do
 	erb :index
 end
+
+get '/users/new' do
+	# @user = User.new
+	erb :"users/new"
+end
+
+# get login
+
+# post signup
+post '/users' do
+	# create a new user
+	# redirect to homepage
+	user = User.create(params[:user])
+	session[:user_id] = user.id
+	redirect '/hugs'
+end
+
+# post login
+
+# homepage
+get '/hugs' do
+	erb :"hugs/index"
+end
