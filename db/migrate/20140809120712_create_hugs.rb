05a9a0547948d2ef5bbd6519185a0a0848e5df08
jermspeaks <jermspeaks@gmail.com>
2014-08-09 12:47:04 -0500

    added appointment times and special requests

--- db/migrate/20140809120712_create_hugs.rb
+++ db/migrate/20140809120712_create_hugs.rb
@@ -1,8 +1,10 @@
 class CreateHugs < ActiveRecord::Migration
   def change
   	create_table :hugs do |t|
-  		t.integer :receiver_id
-  		t.integer :giver_id
+  		t.integer 	:receiver_id
+  		t.integer 	:giver_id
+  		t.datetime 	:appointment
+  		t.text			:special_requests
 
   		t.timestamps
   	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 a = User.create(username: "admin", email: "control@usa.gov", hugger_type: "bear", description: "I like giving bear hugs. I enjoy my hugger to be a smaller person than me", password: "passwordusa", password_confirmation: "passwordusa")
 b = User.create(username: "needy", email: "iluvit@gmail.com", hugger_type: "hug activist", description: "Hugs make the world a better place", password: "passwordusa", password_confirmation: "passwordusa")
-Hug.create(receiver: a, giver: b)
+Hug.create(receiver: a, giver: b, appointment: Time.now, special_requests: "Big Body Needed")
